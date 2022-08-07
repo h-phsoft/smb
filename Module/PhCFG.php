d5f4fcc3fd1fdf51f6373cc659b397ee99b55463
PhSoft <h.phsoft@gmail.com>
2022-08-07 15:58:22 +0300

    Cnage Configuration

--- Module/PhCFG.php
+++ Module/PhCFG.php
@@ -1,31 +1,15 @@
 <?php
 
 define("PHS_VERSION", '1.22 build: 0507.0850');
-/*
-  define("PH_CONN_HOST", 'localhost');
-  define("PH_CONN_PORT", 3306);
-  define("PH_CONN_USER", 'phsoftme_dml');
-  define("PH_CONN_PASS", 'DML@Ph$0ft97135');
 
-  define('PHS_SMB_ADMIN', 'phsoftme_smb_admin');
-  define('PHS_SMB_ADMIN_FULL', PHS_SMB_ADMIN . '.');
-  define('PHS_DEFAULT_COPY', 'sparkle');
-  define('PHS_URI_IDX', 1);
-  define('PHS_SERVER_ROOT_PATH', './../../');
-  define('PHS_ROOT_PATH', '/sparkle/');
-  define('PHS_MEDIA_PATH', PHS_ROOT_PATH . 'assets/media/');
-  define('PHS_MEDIA_COPY_PATH', PHS_ROOT_PATH . 'assets/media/{{COPY}}/');
-  define('PHS_HOST_URL', 'https://www.phsoft.me/sparkle/');
-  define('PHS_GUID_KEY', 'PhsSMB');
- */
 define("PH_CONN_HOST", 'localhost');
 define("PH_CONN_PORT", 3306);
-define("PH_CONN_USER", 'root');
-define("PH_CONN_PASS", 'RootPass');
+define("PH_CONN_USER", 'dml');
+define("PH_CONN_PASS", '');
 
-define('PHS_SMB_ADMIN', 'phs_smb_admin');
+define('PHS_SMB_ADMIN', 'smb_admin');
 define('PHS_SMB_ADMIN_FULL', PHS_SMB_ADMIN . '.');
-define('PHS_DEFAULT_COPY', 'sparkle');
+define('PHS_DEFAULT_COPY', 'customername');
 define('PHS_URI_IDX', 1);
 define('PHS_SERVER_ROOT_PATH', './../../');
 define('PHS_ROOT_PATH', '/smb21/');
@@ -33,4 +17,3 @@ define('PHS_MEDIA_PATH', PHS_ROOT_PATH . 'assets/media/');
 define('PHS_MEDIA_COPY_PATH', PHS_ROOT_PATH . 'assets/media/{{COPY}}/');
 define('PHS_HOST_URL', 'https://www.phsoft.me');
 define('PHS_GUID_KEY', 'PhsSMB');
-
